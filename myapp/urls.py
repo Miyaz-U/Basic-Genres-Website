3fe58cd4f2623a0ed4144ceba23e89a8bddbdb0b
Miyaz U <miyazumare@gmail.com>
2025-06-10 13:11:52 +0530

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('genres/',include('genres.urls')),
+    path('',include('genres.urls')),
     path('admin/', admin.site.urls),
 ]
